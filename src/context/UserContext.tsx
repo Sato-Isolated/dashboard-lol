9ea66038a67383aa4058f61f30700c4b585a76a2
MindLated <ismyskyllz@gmail.com>
2025-05-17 15:29:13 +0200

    Add UserContext and integrate with RootLayout and Home component

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useState } from 'react'
+'use client';
+import React, { createContext, useContext, useState } from "react";
 
 // Interface user defines the structure of the user object
 // It contains the following properties:
@@ -6,9 +7,9 @@ import React, { createContext, useContext, useState } from 'react'
 // - tagline: string - The tagline of the summoner
 // - region: string - The region of the summoner
 interface User {
-  summonerName: string
-  tagline: string
-  region: string
+  summonerName: string;
+  tagline: string;
+  region: string;
 }
 // The UserContextType interface defines the structure of the context
 // It contains the following properties:
@@ -16,34 +17,36 @@ interface User {
 // - setUser: React.Dispatch<React.SetStateAction<User>> - A function to update the user object
 // The setUser function is used to update the user object in the context
 interface UserContextType {
-  user: User
-  setUser: React.Dispatch<React.SetStateAction<User>>
+  user: User;
+  setUser: React.Dispatch<React.SetStateAction<User>>;
 }
 // The UserContext is created using React's createContext function
-const UserContext = createContext<UserContextType | undefined>(undefined)
+const UserContext = createContext<UserContextType | undefined>(undefined);
 
 // The UserProvider component is a context provider that wraps its children with the UserContext
 // It provides the user object and the setUser function to its children
-export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const UserProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [user, setUser] = useState<User>({
-    summonerName: '',
-    tagline: '',
-    region: 'euw1',
-  })
+    summonerName: "",
+    tagline: "",
+    region: "euw1",
+  });
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
-  )
-}
+  );
+};
 
 // The useUser hook is a custom hook that allows components to access the UserContext
 // It throws an error if the hook is used outside of the UserProvider
 export const useUser = (): UserContextType => {
-  const context = useContext(UserContext)
+  const context = useContext(UserContext);
   if (!context) {
-    throw new Error('useUser must be used within a UserProvider')
+    throw new Error("useUser must be used within a UserProvider");
   }
-  return context
-}
+  return context;
+};
