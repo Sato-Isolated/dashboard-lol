2ac8de2fe54fff286cc6c1d7c45deadbd6b3cb7c
MindLated <ismyskyllz@gmail.com>
2025-05-17 15:46:31 +0200

    Refactor package.json and pnpm-lock.yaml; update globals.css to include daisyui plugin; enhance Home component with improved input styling and add submit button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,44 @@
-'use client';
+"use client";
 import { useUser } from "@/context/UserContext";
 
 export default function Home() {
   const { user, setUser } = useUser();
 
   return (
-    <main className="flex flex-col min-h-screen items-center p-24">
+    <main className="flex flex-col gap-2 min-h-screen items-center p-24">
       <div className="flex gap-1">
         <input
           type="text"
           placeholder="Summoners Name"
-          className="border p-2"
+          className="input"
           value={user.summonerName}
           onChange={(e) => setUser({ ...user, summonerName: e.target.value })}
         />
         <input
           type="text"
-          placeholder="Tag ex: #1234"
-          className="border p-2"
+          placeholder="#1234"
+          className="input w-20"
           value={user.tagline}
           onChange={(e) => setUser({ ...user, tagline: e.target.value })}
         />
         <select
           name="region"
           id="region"
-          className="border p-2"
+          className="select w-28"
           value={user.region}
           onChange={(e) => setUser({ ...user, region: e.target.value })}
         >
           <option value="euw1">euw1</option>
           <option value="na1">na1</option>
         </select>
+      </div>
+      <div>
+        <button
+          className="btn btn-outline p-2 w-96"
+          onClick={() => console.log(user)}
+        >
+          Submit
+        </button>
       </div>
     </main>
   );
